8d48a9bd63402f46820438ade21ae001bd59c209
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-01-07 23:21:43 +0800

    Add combo

--- Assets/Scripts/Inputs/LeftRightButtonHandler.cs
+++ Assets/Scripts/Inputs/LeftRightButtonHandler.cs
@@ -9,14 +9,15 @@ namespace Inputs
     public class LeftRightButtonHandler : MonoBehaviour
     {
         [SerializeField] private Grid grid;
-        [SerializeField] private Text scoreText;
+        [SerializeField] private Text scoreText,
+            comboText;
         [SerializeField] private GameObject wrongPanel;
         
-        private int score;
+        private int score, combo;
 
         private void Start()
         {
-            score = 0;
+            score = combo = 0;
         }
 
         /// <summary>
@@ -32,24 +33,47 @@ namespace Inputs
             {
                 grid.DestroyBox();
                 AddScore(10);
+                combo++;
+                SetCombo(true);
             }
             else
             {
+                combo = 0;
+                SetCombo(false);
                 StartCoroutine(ShowWrongPanel(2));
             }
         }
-
+        
+        /// <summary>
+        /// Add score 
+        /// </summary>
+        /// <param name="value">Additional score</param>
         private void AddScore(int value)
         {
-            score += value;
+            score += value + (int)((combo - 0.5)*10);
             scoreText.text = score.ToString();
         }
-
+        
+        /// <summary>
+        /// Penalty if user input is false
+        /// </summary>
+        /// <param name="delayTime"></param>
+        /// <returns>Wait for delay time</returns>
         private IEnumerator ShowWrongPanel(float delayTime)
         {
             wrongPanel.SetActive(true);
             yield return new WaitForSeconds(delayTime);
             wrongPanel.SetActive(false);
+        }
+        
+        /// <summary>
+        /// Set combo text and isActive
+        /// </summary>
+        /// <param name="isActive"></param>
+        private void SetCombo(bool isActive)
+        {
+            comboText.text = $"Combo x{combo}";
+            comboText.gameObject.SetActive(isActive);
         }
     }
 }
