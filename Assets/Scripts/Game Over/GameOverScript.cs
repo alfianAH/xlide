845ff1ae200b82f66e2a135a241f6689afa5f0ac
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-01-30 21:29:59 +0800

    Make Game Over script for score

---
+++ Assets/Scripts/Game Over/GameOverScript.cs
@@ -0,0 +1,24 @@
+﻿using Effect;
+using Score;
+using TMPro;
+using UnityEngine;
+
+namespace Game_Over
+{
+    public class GameOverScript : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private ScoreViewModel scoreViewModel;
+        
+        private int initialScore;
+
+        private void OnEnable()
+        {
+            int targetScore = scoreViewModel.scoreModel.score;
+            StartCoroutine(AddNumberEffect.AddNumber(initialScore,
+                targetScore, scoreText, 0.02f));
+
+            initialScore = targetScore;
+        }
+    }
+}
